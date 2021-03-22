8038da3a30da6a98944917125b29705e59448faa
JimmyNeutron228 <TlVa474367636656565>
2021-03-22 18:50:40 +0300

    login

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from data import db_session
 from data.users import Users
 from data.ads import Ads
 from data.favorites import Favorites
+from forms.login import LoginForm
 from flask import Flask, request, abort
 from flask import render_template, redirect
 from flask_login import LoginManager, login_user
@@ -35,6 +36,21 @@ def load_user(user_id):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def entrance():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(Users).filter(Users.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.is_remember_me.data)
+            return redirect("/")
+        return render_template('entrance.html',
+                               message='Wrong login or password',
+                               form=form)
+    return render_template('entrance.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
